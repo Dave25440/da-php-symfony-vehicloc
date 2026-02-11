8e9643565ddd387b15758bc3246dc58a4a91af4f
David Horès <hores.d@gmail.com>
2026-02-11 18:47:44 +0100

    Create CarController with Doctrine

---
+++ src/Controller/CarController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CarRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CarController extends AbstractController
+{
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(CarRepository $carRepository): Response
+    {
+        $cars = $carRepository->findAll();
+
+        return $this->render('car/index.html.twig', [
+            'cars' => $cars,
+        ]);
+    }
+}
