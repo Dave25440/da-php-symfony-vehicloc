b13421bda1dbf5774d9ec1b1a01201d6219cb3aa
David Horès <hores.d@gmail.com>
2026-02-11 14:58:31 +0100

    Create Car Entity with Doctrine

---
+++ src/Entity/Car.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+class Car
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'id')]
+    private ?int $id = null;
+
+    #[ORM\Column(name: 'name', length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(name: 'description', type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(name: 'monthly_price', type: 'decimal', precision: 8, scale: 2)]
+    private ?string $monthlyPrice = null;
+
+    #[ORM\Column(name: 'daily_price', type: 'decimal', precision: 8, scale: 2)]
+    private ?string $dailyPrice = null;
+
+    #[ORM\Column(name: 'seat_number')]
+    private ?int $seatNumber = null;
+
+    #[ORM\Column(name: 'manual_transmission')]
+    private ?bool $manualTransmission = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getMonthlyPrice(): ?string
+    {
+        return $this->monthlyPrice;
+    }
+
+    public function setMonthlyPrice(string $monthlyPrice): static
+    {
+        $this->monthlyPrice = $monthlyPrice;
+
+        return $this;
+    }
+
+    public function getDailyPrice(): ?string
+    {
+        return $this->dailyPrice;
+    }
+
+    public function setDailyPrice(string $dailyPrice): static
+    {
+        $this->dailyPrice = $dailyPrice;
+
+        return $this;
+    }
+
+    public function getSeatNumber(): ?int
+    {
+        return $this->seatNumber;
+    }
+
+    public function setSeatNumber(int $seatNumber): static
+    {
+        $this->seatNumber = $seatNumber;
+
+        return $this;
+    }
+
+    public function getManualTransmission(): ?bool
+    {
+        return $this->manualTransmission;
+    }
+
+    public function setManualTransmission(bool $manualTransmission): static
+    {
+        $this->manualTransmission = $manualTransmission;
+
+        return $this;
+    }
+}
