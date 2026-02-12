8b63dd2cd8f0e20bac1ba2d78db7528ae775a459
David Horès <hores.d@gmail.com>
2026-02-12 14:21:13 +0100

    Add new method with new route to CarController and update base template

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Car;
+use App\Form\CarType;
 use App\Repository\CarRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -30,6 +32,26 @@ final class CarController extends AbstractController
 
         return $this->render('car/show.html.twig', [
             'car' => $car,
+        ]);
+    }
+
+    #[Route('/car/new', name: 'app_car_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $car = new Car();
+        $form = $this->createForm(CarType::class, $car);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($car);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_car', ['id' => $car->getId()]);
+        }
+
+        return $this->render('car/new.html.twig', [
+            'form' => $form,
         ]);
     }
 
